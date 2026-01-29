b480ea32de5ceeeedf27b436b6a0a75e38bb9b58
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 17:17:05 +0200

    test: Add base TestCase for Laravel bridge tests

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fyennyi\AsyncCache\Bridge\Laravel\Tests;
+
+use Fyennyi\AsyncCache\Bridge\Laravel\AsyncCacheServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, string>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            AsyncCacheServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default configuration for tests
+        $app['config']->set('cache.default', 'array');
+    }
+}
